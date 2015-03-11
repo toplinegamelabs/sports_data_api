7b715e36070ea67207c6ff238118079ec85214d0
Joe Trdinich <joe.trdinich@toplinegamelabs.com>
2015-03-11 16:29:43 -0700

    Remove 'total_' from all stat names

--- lib/sports_data_api/stats.rb
+++ lib/sports_data_api/stats.rb
@@ -6,13 +6,14 @@ module SportsDataApi
         self.class.class_eval { attr_reader :"statistics" }
         stats.each_pair do |parent_k, parent_v|
           if parent_v.is_a? Hash
+            parent_k = parent_k == 'total' ? '' : "#{parent_k}_"
             parent_v.each_pair do |child_k, child_v|
               if child_v.is_a? Hash
                 child_v.each_pair do |grandchild_k, grandchild_v|
-                  stats_ivar["#{parent_k}_#{child_k}_#{grandchild_k}".to_sym] = grandchild_v
+                  stats_ivar["#{parent_k}#{child_k}_#{grandchild_k}".to_sym] = grandchild_v
                 end
               else
-                stats_ivar["#{parent_k}_#{child_k}".to_sym] = child_v
+                stats_ivar["#{parent_k}#{child_k}".to_sym] = child_v
               end
             end
           else
